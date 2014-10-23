5f75b7408c79cafa9a7eec7cd126972a41d150d3
Marek Siarkowicz <mareksiarkowicz@gmail.com>
2014-10-23 02:31:36 +0200

    Implement window.

--- include/view/window.h
+++ include/view/window.h
@@ -1,16 +1,31 @@
 #ifndef WINDOW_H_
 #define WINDOW_H_
 
+#include <cstdlib>
+
+#include <GLFW/glfw3.h>
+
 
 class Window
 {
 private:
-	int width;
-	int height;
+    GLFWwindow* window_handle;
+    virtual void draw(int, int) = 0;
 
 public:
-	Window(int, int);
+	Window(const char*, int, int);
+    virtual ~Window();
+    void show();
 };
 
+
+class MainWindow : public Window
+{
+private:
+    virtual void draw(int, int);
+
+public:
+    MainWindow(const char *, int, int);
+};
 
 #endif /* WINDOW_H_ */
---
+++ src/view/window.cpp
@@ -0,0 +1,40 @@
+#include "view/window.h"
+
+Window::Window(const char* name, int width, int height)
+{
+    this->window_handle = glfwCreateWindow(width, height, name, NULL, NULL);
+    if (!this->window_handle)
+    {
+        glfwTerminate();
+        std::exit(2);
+    }
+}
+
+Window::~Window()
+{
+    glfwDestroyWindow(this->window_handle);
+}
+
+void Window::show()
+{
+    glfwMakeContextCurrent(this->window_handle);
+    glfwSwapInterval(1);
+    while (!glfwWindowShouldClose(this->window_handle))
+    {
+        int width, height;
+        glfwGetFramebufferSize(this->window_handle, &width, &height);
+        this->draw(width, height);
+        glfwPollEvents();
+    }
+}
+
+MainWindow::MainWindow(const char* name, int width, int height)
+    : Window(name, width, height)
+{
+
+}
+
+void MainWindow::draw(int width, int height)
+{
+
+}
