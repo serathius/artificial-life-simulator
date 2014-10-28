7d42ad8fefe2b2f91fc2d9f5606f6253e6eb0f9b
matfil <mateusz.filipiuk@gmail.com>
2014-10-28 18:11:11 +0100

    Add ViewModel class

---
+++ include/view/viewmodel.h
@@ -0,0 +1,38 @@
+#ifndef VIEWMODEL_H_
+#define VIEWMODEL_H_
+
+#include <vector>
+
+#include "model/world.h"
+
+
+class ViewModel
+{
+private:
+public:
+    virtual void draw() = 0;
+};
+
+class CircleShape : public ViewModel
+{
+private:
+    const float x;
+    const float y;
+    const float z;
+    const float r;
+public:
+    CircleShape(float, float, float, float);
+};
+
+class BoxShape : public ViewModel
+{
+private:
+    const float x;
+    const float y;
+    const float z;
+public:
+    BoxShape(float, float, float);
+};
+
+
+#endif /* VIEWMODEL_H_ */
