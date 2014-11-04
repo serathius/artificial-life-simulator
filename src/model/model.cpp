9c557bd76b758e4c81d1189fa7f88c00a6576cba
Marek Siarkowicz <mareksiarkowicz@gmail.com>
2014-11-04 19:42:41 +0100

    Implement mode get_newt_event_time.

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -9,6 +9,19 @@ void Model::update(const RealTime& realtime)
     }
 }
 
+const RealTime Model::get_next_event_time() const
+{
+    AbsoluteTime earliest_event_time =
+        (*this->event_objects.begin())->get_next_event_time();
+    for (auto event_object: this->event_objects)
+    {
+        auto event_time = event_object->get_next_event_time();
+        if (earliest_event_time > event_time)
+            earliest_event_time = event_time;
+    }
+    return this->clock->to_realtime(earliest_event_time);
+}
+
 EventObjectCollection::Iterator EventObjectCollection::begin() const
 {
     return this->event_objects.begin();
