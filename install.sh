f1b6d40084b9f917a72459c354f025ae10a53558
Marek Siarkowicz <mareksiarkowicz@gmail.com>
2015-01-13 14:15:51 +0100

    Migrate to cmake.

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+
+EXTERNAL_DIR=external
+EXTERNAL_LIBDIR=external/lib
 PROJECTDIR=$(pwd)
+
 if [ ! -e $PROJECTDIR/external/lib/libglfw3.a ]; then
     if [ ! -d /tmp/glfw-3.0.4 ]; then
         if [ ! -e /tmp/glfw-3.0.4.zip ]; then
@@ -8,9 +12,11 @@ if [ ! -e $PROJECTDIR/external/lib/libglfw3.a ]; then
         unzip /tmp/glfw-3.0.4.zip -d /tmp 1> /dev/null
     fi
     cd /tmp/glfw-3.0.4
-        cmake -DCMAKE_INSTALL_PREFIX=. .
-        make
-        make install
-    cp /tmp/glfw-3.0.4/lib/libglfw3.a $PROJECTDIR/external/lib
+    cmake -DCMAKE_INSTALL_PREFIX=. .
+    make
+    make install
+    cd $PROJECTDIR
+    mkdir -p $EXTERNAL_DIR $EXTERNAL_LIBDIR
+    cp /tmp/glfw-3.0.4/lib/libglfw3.a $PROJECTDIR/$EXTERNAL_LIBDIR/
     cp -r /tmp/glfw-3.0.4/include $PROJECTDIR/external/
 fi
--- run_tests.sh
+++ run_tests.sh
@@ -1,24 +1,26 @@
-#!/bin/sh
-./install.sh
-if [ ! -e external/lib/libgtest.a ]; then
-    if [ ! -d /tmp/gtest-1.7.0 ]; then
-        if [ ! -e /tmp/gtest-1.7.0.zip ]; then
-            wget https://googletest.googlecode.com/files/gtest-1.7.0.zip -P /tmp 1> /dev/null
+#!/bin/bash
+
+TEMP_DIR=/tmp
+BUILD_DIR=build
+GMOCK="gmock-1.7.0"
+DEPENDENCIES=($GMOCK)
+declare -A URLS
+URLS["$GMOCK"]="https://googlemock.googlecode.com/files/gmock-1.7.0.zip"
+declare -A RENAME
+RENAME["$GMOCK"]="gmock"
+
+mkdir -p $BUILD_DIR
+for DEPENDENCY in ${DEPENDENCIES[*]}; do
+    if [ ! -d $BUILD_DIR/${RENAME[$DEPENDENCY]} ]; then
+        if [ ! -e "$TEMP_DIR/$DEPENDENCY.zip" ]; then
+            wget -O "$TEMP_DIR/$DEPENDENCY.zip" ${URLS[$DEPENDENCY]} 1> /dev/null
         fi
-        unzip /tmp/gtest-1.7.0.zip -d /tmp 1> /dev/null
+
+        unzip $TEMP_DIR/$DEPENDENCY.zip -d $BUILD_DIR/ 1> /dev/null
+        mv $BUILD_DIR/$DEPENDENCY $BUILD_DIR/${RENAME[$DEPENDENCY]}
     fi
-    if [ ! -d /tmp/gmock-1.7.0 ]; then
-        if [ ! -e /tmp/gmock-1.7.0.zip ]; then
-            wget https://googlemock.googlecode.com/files/gmock-1.7.0.zip -P /tmp 1> /dev/null
-        fi
-        unzip /tmp/gmock-1.7.0.zip -d /tmp 1> /dev/null
-    fi
-    g++ -isystem /tmp/gtest-1.7.0/include -isystem /tmp/gmock-1.7.0/include -I /tmp/gtest-1.7.0 -I /tmp/gmock-1.7.0 -c /tmp/gtest-1.7.0/src/gtest-all.cc -o /tmp/gtest-1.7.0/gtest-all.o
-    g++ -isystem /tmp/gtest-1.7.0/include -isystem /tmp/gmock-1.7.0/include -I /tmp/gtest-1.7.0 -I /tmp/gmock-1.7.0 -c /tmp/gmock-1.7.0/src/gmock-all.cc -o /tmp/gmock-1.7.0/gmock-all.o
-    ar -rv external/lib/libgtest.a /tmp/gtest-1.7.0/gtest-all.o /tmp/gmock-1.7.0/gmock-all.o
-    cp -r /tmp/gmock-1.7.0/include external/
-    cp -r /tmp/gtest-1.7.0/include external/
-fi
+done
 ./install.sh
-make tests
-bin/tests.o
+cmake .
+make unittests
+./unittests
