e4b74912965ac2f70edf0da923e01693baeb35d5
Marek Siarkowicz <mareksiarkowicz@gmail.com>
2015-01-17 19:20:26 +0100

    Fix organism drawing.

--- src/model/organism.cpp
+++ src/model/organism.cpp
@@ -15,15 +15,16 @@ void Organism::draw()
   float x = coordinates_.get_x();
   float y = coordinates_.get_y();
   float r = 0.2f;
-
   glColor3f(1.0f, 0.f, 0.f);
-  glRotatef(direction_.get_angle(), 0, 0, 1);
+  glTranslatef(x, y, 0.f);
+  glRotatef(direction_.get_angle(), 0, 0, -1);
   glBegin(GL_TRIANGLES);
-    glVertex2f(x - r / 2, y - r * (float) sqrt(3) / 6);
-    glVertex2f(x, y + r * (float) sqrt(3) / 3);
-    glVertex2f(x + r / 2, y - r * (float) sqrt(3) / 6);
+    glVertex2f(- r / 2, - r * (float) sqrt(3) / 6);
+    glVertex2f(0.f, r * (float) sqrt(3) / 3);
+    glVertex2f(r / 2, - r * (float) sqrt(3) / 6);
   glEnd();
-  glRotatef(-direction_.get_angle(), 0, 0, 1);
+  glRotatef(-direction_.get_angle(), 0, 0, -1);
+  glTranslatef(-x, -y, 0.f);
 }
 
 void Organism::update(AbsoluteTime const &time)
