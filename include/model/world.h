06726569b9499ead77df214ffb056942cb5debb7
Marek Siarkowicz <mareksiarkowicz@gmail.com>
2015-01-24 20:03:20 +0100

    Refactor.

--- include/model/world.h
+++ include/model/world.h
@@ -11,14 +11,14 @@ class World;
 class World
 {
 private:
-    WorldObjectsCollection world_objects_;
-    EventObjectCollection event_objects_;
+  WorldObjectsCollection world_objects_;
+  EventObjectCollection event_objects_;
 
 public:
-    World(Model* const);
-    void update(const AbsoluteTime&);
-    const AbsoluteTime get_next_event_time() const;
-    const std::vector<std::shared_ptr<WorldObjectView>> get_objects() const;
+  World(Model* const);
+  void update(const AbsoluteTime&);
+  const AbsoluteTime get_next_event_time() const;
+  const std::vector<std::shared_ptr<WorldObjectView>> get_objects() const;
 };
 
 #endif
